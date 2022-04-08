0bdea89db24908a4ac8d6215306f3e72aeec85b8
Ronan O'Keeffe <ronan.okeeffe1@mycit.ie>
2022-04-08 15:46:03 +0100

    Sprint 5 conversation, accommodation added

--- actions/actions.py
+++ actions/actions.py
@@ -10,6 +10,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+# FUNCTIONALITY ACTIONS
 def getText(key, lang):
     # Define the flag to indicate if the text was found in the csv
     check = False
@@ -37,7 +38,6 @@ def getText(key, lang):
 
     return res
 
-# Actions for fallback option
 class ActionDefaultFallback(Action):
     def name(self) -> Text:
         return "action_default_fallback"
@@ -47,7 +47,7 @@ class ActionDefaultFallback(Action):
         dispatcher.utter_message(text=getText('default_fallback', user_lang))
         return []
 
-# Actions for onboarding conversation
+# ACTIONS FOR ONBOARDING STORY
 class ActionLanguageSelect(Action):
     def name(self) -> Text:
         return "action_language_select"
@@ -72,7 +72,7 @@ class ActionLanguageSelect(Action):
         )
         return []
 
-# Actions for main menu
+# ACTIONS FOR MAIN MENU STORY
 class ActionUtterMainMenu(Action):
     def name(self) -> Text:
         return "action_utter_main_menu"
@@ -86,7 +86,7 @@ class ActionUtterMainMenu(Action):
             }])
         return []
 
-# Actions for Where to get help
+# ACTIONS FOR WHERE TO GET HELP STORIES
 class ActionUtterYourLocalCitizens(Action):
     def name(self) -> Text:
         return "action_utter_your_local_citizens"
@@ -122,6 +122,182 @@ class ActionUtterShowLocalCentre(Action):
         return "action_utter_show_local_centre"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message(text=getText('utter_show_local_centre', 'en'))
+        dispatcher.utter_message(text=getText('utter_show_local_centre', tracker.get_slot("user_lang")))
         return []
 
+# ACTIONS FOR ACCOMMODATION STORIES
+## Menu for Accommodation navigation
+class ActionUtterTheFirstChallenge(Action):
+    def name(self) -> Text:
+        return "action_utter_the_first_challenge"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_the_first_challenge', user_lang), buttons=[{
+            "title": getText('social_housing', user_lang),
+            "payload": "/social_housing"
+        },{
+            "title": getText('private_housing', user_lang),
+            "payload": "/private_housing"
+        },{
+            "title": getText('homelessness', user_lang),
+            "payload": "/homelessness"
+        },{
+            "title": getText('help_with_paying', user_lang),
+            "payload": "/help_with_paying"
+        },{
+            "title": getText('housing_checklist', user_lang),
+            "payload": "/housing_checklist"
+        },{
+            "title": getText('main_menu', user_lang),
+            "payload": "/main_menu"
+        }])
+        return []
+
+## Public housing
+class ActionUtterInIrelandCity(Action):
+    def name(self) -> Text:
+        return "action_utter_in_ireland_city"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_in_ireland_city', user_lang))
+        dispatcher.utter_message(text=getText('utter_housing_associations_and', user_lang))
+        return []
+
+class ActionUtterNotEveryoneHas(Action):
+    def name(self) -> Text:
+        return "action_utter_not_everyone_has"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_not_everyone_has', user_lang))
+        dispatcher.utter_message(text=getText('utter_housing_associations_and_2', user_lang), buttons=[{
+            "title": getText('what_points_do', user_lang),
+            "payload": "/what_points_do"
+        }])
+        return []
+
+class ActionUtterGeneralPoints(Action):
+    def name(self) -> Text:
+        return "action_utter_general_points"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_general_points_1', user_lang))
+        dispatcher.utter_message(text=getText('utter_general_points_2', user_lang))
+        dispatcher.utter_message(text=getText('utter_general_points_3', user_lang))
+        dispatcher.utter_message(text=getText('utter_general_points_4', user_lang))
+        dispatcher.utter_message(text=getText('utter_general_points_5', user_lang))
+        dispatcher.utter_message(text=getText('utter_general_points_6', user_lang))
+        dispatcher.utter_message(text=getText('utter_general_points_7', user_lang))
+        dispatcher.utter_message(text=getText('utter_general_points_8', user_lang))
+
+        return []
+
+## Private housing
+class ActionUtterIfYouDo(Action):
+    def name(self) -> Text:
+        return "action_utter_if_you_do"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_if_you_do', user_lang))
+        dispatcher.utter_message(text=getText('utter_the_private_residential', user_lang))
+        dispatcher.utter_message(text=getText('utter_you_can_also', user_lang))
+        return []
+
+class ActionUtterTheCostOf(Action):
+    def name(self) -> Text:
+        return "action_utter_the_cost_of"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_the_cost_of', user_lang))
+        dispatcher.utter_message(text=getText('utter_you_will_usually', user_lang))
+        dispatcher.utter_message(text=getText('utter_both_threshold_and', user_lang), buttons=[{
+            "title": getText('what_documents_might', user_lang),
+            "payload": "/what_documents_might"
+        }])
+        return []
+
+class ActionUtterTheLandlordMay(Action):
+    def name(self) -> Text:
+        return "action_utter_the_landlord_may"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_the_landlord_may_1', user_lang))
+        dispatcher.utter_message(text=getText('utter_the_landlord_may_2', user_lang))
+        dispatcher.utter_message(text=getText('utter_the_landlord_may_3', user_lang))
+        dispatcher.utter_message(text=getText('utter_the_landlord_may_4', user_lang))
+        return []
+
+## Homelessness
+class ActionUtterIfYouOr(Action):
+    def name(self) -> Text:
+        return "action_utter_if_you_or"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_if_you_or_1', user_lang))
+        dispatcher.utter_message(text=getText('utter_if_you_or_2', user_lang))
+        dispatcher.utter_message(text=getText('utter_if_you_or_3', user_lang))
+        dispatcher.utter_message(text=getText('utter_if_you_or_4', user_lang))
+        dispatcher.utter_message(text=getText('utter_if_you_or_5', user_lang))
+        dispatcher.utter_message(text=getText('utter_if_you_or_6', user_lang))
+        dispatcher.utter_message(text=getText('utter_if_you_or_7', user_lang))
+        dispatcher.utter_message(text=getText('utter_if_you_or_8', user_lang))
+
+        return []
+
+class ActionUtterThereAreTwo(Action):
+    def name(self) -> Text:
+        return "action_utter_there_are_two"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_there_are_two_1', user_lang))
+        dispatcher.utter_message(text=getText('utter_there_are_two_2', user_lang))
+        dispatcher.utter_message(text=getText('utter_there_are_two_3', user_lang), buttons=[{
+            "title": getText('housing_assistance_payment', user_lang),
+            "payload": "/housing_assistance_payment"
+        },{
+            "title": getText('rent_supplement', user_lang),
+            "payload": "/rent_supplement"
+        }])
+        return []
+
+class ActionUtterHapIsA(Action):
+    def name(self) -> Text:
+        return "action_utter_hap_is_a"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_hap_is_a', user_lang))
+        dispatcher.utter_message(text=getText('utter_hap_will_mean', user_lang))
+        dispatcher.utter_message(text=getText('utter_under_hap_you', user_lang))
+        dispatcher.utter_message(text=getText('utter_you_must_pay', user_lang))
+        return []
+
+class ActionUtterIfYouLive(Action):
+    def name(self) -> Text:
+        return "action_utter_if_you_live"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        user_lang = tracker.get_slot("user_lang")
+        dispatcher.utter_message(text=getText('utter_if_you_live', user_lang))
+        dispatcher.utter_message(text=getText('utter_rent_supplement_is_1', user_lang))
+        dispatcher.utter_message(text=getText('utter_rent_supplement_is_2', user_lang))
+        dispatcher.utter_message(text=getText('utter_rent_supplement_is_3', user_lang))
+        dispatcher.utter_message(text=getText('utter_rent_supplement_is_4', user_lang))
+        dispatcher.utter_message(text=getText('utter_you_can_find', user_lang))
+        return []
+
+class ActionUtterHousingChecklist(Action):
+    def name(self) -> Text:
+        return "action_utter_housing_checklist"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text=getText('', 'en'))
+        return []
--- data/stories.md
+++ data/stories.md
@@ -1,44 +1,14 @@
-<!-- ## happy path
-* greet
-  - utter_greet
-* mood_great
-  - utter_happy
-
-## sad path 1
-* greet
-  - utter_greet
-* mood_unhappy
-  - action_hello_world
-  - utter_did_that_help
-* affirm
-  - utter_happy
-
-## sad path 2
-* greet
-  - utter_greet
-* mood_unhappy
-  - utter_cheer_up
-  - utter_did_that_help
-* deny
-  - utter_goodbye
-
-## say goodbye
-* goodbye
-  - utter_goodbye
-
-## bot challenge
-* bot_challenge
-  - utter_iamabot -->
-
+<!-- ONBOARDING STORIES -->
 ## Onboarding
 * greet
+* choose_lang
   - action_language_select
 * lang_choice{"user_lang":"{user_lang}"}
   - slot{"user_lang":"{user_lang}"}
   - action_utter_main_menu
 <!-- Show main menu -->
 
-## Show main menu
+## Where to get help
 * conversation_where_to_get
   - action_utter_your_local_citizens
 > where_to_get_choice_1
@@ -54,5 +24,73 @@
 > where_to_get_choice_1
 * affirm
   - action_utter_show_local_centre
+  - action_utter_main_menu
+<!-- Show main menu -->
+
+<!-- ACCOMMODATION STORIES -->
+## Accommodation
+* conversation_accommodation
+  - action_utter_the_first_challenge
+> accommodation_choice_1
+
+## Accommodation: Social Housing
+> accommodation_choice_1
+* social_housing
+  - action_utter_in_ireland_city
+* do_i_have
+  - action_utter_not_everyone_has
+* what_points_do
+  - action_utter_general_points
+  - action_utter_the_first_challenge
+<!-- Show accommodation menu -->
+
+## Accommodation: Private Housing
+> accommodation_choice_1
+* private_housing
+  - action_utter_if_you_do
+* how_much_will
+  - action_utter_the_cost_of
+* what_documents_might
+  - action_utter_the_landlord_may
+  - action_utter_the_first_challenge
+<!-- Show accommodation menu -->
+
+## Accommodation: Homelessness
+> accommodation_choice_1
+* homelessness
+  - action_utter_if_you_or
+  - action_utter_the_first_challenge
+<!-- Show accommodation menu -->
+
+## Accommodation: Help with paying your rent
+> accommodation_choice_1
+* help_with_paying
+  - action_utter_there_are_two
+> accommodation_choice_2
+
+## Accommodation: Help with paying your rent -> HAP
+> accommodation_choice_2
+* housing_assistance_payment
+  - action_utter_hap_is_a
+  - action_utter_the_first_challenge
+<!-- Show accommodation menu -->
+
+## Accommodation: Help with paying your rent -> Rent supplement
+> accommodation_choice_2
+* rent_supplement
+  - action_utter_if_you_live
+  - action_utter_the_first_challenge
+<!-- Show accommodation menu -->
+
+## Accomodation: Housing checklist
+> accommodation_choice_1
+* housing_checklist
+  - action_utter_housing_checklist
+  - action_utter_the_first_challenge
+<!-- Show accommodation menu -->
+
+## Accommodation: Main Menu
+> accommodation_choice_1
+* main_menu
   - action_utter_main_menu
 <!-- Show main menu -->
